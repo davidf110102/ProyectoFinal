0a6fd31867a25d77f5fcbf9c8f28ed5c4c6839a0
David Flores <eflores9091@uta.edu.ec>
2022-07-16 21:38:44 -0500

    Se agregaron los atributos y metodos de la clase IngresoAltas

--- ProyectoManejo/src/proyectomanejo/IngresoAltas.java
+++ ProyectoManejo/src/proyectomanejo/IngresoAltas.java
@@ -12,6 +12,126 @@ import java.util.Scanner;
  * @author DELLWIN10
  */
 public class IngresoAltas {
+    
+        String cedula;
+    String nombre;
+    String apellido;
+    String direccion;
+    String telefono;
+    String observacion;
+    String presion;
+    String pulso;
+    String temperatura;
+    String fecha;
+    String razon;
+    String cedmedico;
+    String medico;
+    String habitacion;
+    String cama;
+    String otro;
+    Boolean ot;
+    Boolean ce = true;
+    public static String pasahabitacion;
+    public static String pasacama;
+
+    //cedula
+    public void setingresocedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getingresocedula() {
+        return cedula;
+    }
+
+    //nombre
+    public void setingresonombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getingresonombre() {
+        return nombre;
+    }
+
+    //apellido
+    public void setingresoapellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getingresoapellido() {
+        return apellido;
+    }
+
+    //direccion
+    public void setingresodireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getingresodireccion() {
+        return direccion;
+    }
+
+    //telefono
+    public void setingresotelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getingresotelefono() {
+        return telefono;
+    }
+
+    //ingreso
+    public void setingresoobservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    public String getingresoobservacion() {
+        return observacion;
+    }
+
+    //presion
+    public void setingresopresion(String presion) {
+        this.presion = presion;
+    }
+
+    public String getingresopresion() {
+        return presion;
+    }
+
+    //pulso
+    public void setingresopulso(String pulso) {
+        this.pulso = pulso;
+    }
+
+    public String getingresopulso() {
+        return pulso;
+    }
+
+    //temperatura
+    public void setingresotemperatura(String temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public String getingresotemperatura() {
+        return temperatura;
+    }
+
+    //fecha de ingreso
+    public void setingresofecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getingresofecha() {
+        return fecha;
+    }
+
+    //razón del ingreso
+    public void setingresorazon(String razon) {
+        this.razon = razon;
+    }
+
+    public String getingresorazon() {
+        return razon;
+    }
  
     
 }
