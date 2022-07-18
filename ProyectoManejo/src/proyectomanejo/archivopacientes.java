384eea87ec85bfa756549574e61be7fa79867d8e
AlexiP18 <apoaquiza6287@uta.edu.ec>
2022-07-17 20:45:50 -0500

    Se creo la clase archivopacientes con sus atributos

---
+++ ProyectoManejo/src/proyectomanejo/archivopacientes.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectomanejo;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class archivopacientes {
+    public static String ced;
+public static String hospitalizado;
+public static String pasahabitacion;
+public static String pasacama;
+String cont[] =new String[20];
+}
