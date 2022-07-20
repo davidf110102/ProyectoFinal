51451c433961f17342821f795ea18ee0b24a5c21
Lizard <Lizard@Lizard>
2022-07-19 19:33:20 -0500

    Se realizo el metodo buscarmedicos

--- ProyectoManejo/src/proyectomanejo/archivomedicos.java
+++ ProyectoManejo/src/proyectomanejo/archivomedicos.java
@@ -5,9 +5,12 @@
  */
 package proyectomanejo;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +18,7 @@ import java.io.FileWriter;
  */
 public class archivomedicos {
     
-            public void escribirpersona(Persona nueva) {
+    public void escribirpersona(Persona nueva) {
         try {
             File f = new File("medicos.txt");
             FileWriter fw;
@@ -38,7 +41,40 @@ public class archivomedicos {
             System.out.println(e);
         }
     }
+    
+    //BUSCAR MEDICOS
+    public void buscarmedicos() {
+        try {
+            Integer x = 0;
+            Scanner tecla = new Scanner(System.in);
+            File f = new File("medicos.txt");
+            if (f.exists()) {
+                FileReader fr = new FileReader(f);
+                BufferedReader br = new BufferedReader(fr);
+                String linea, buscacedula;
+                System.out.print("Ingrese la cédula: ");
+                buscacedula = (tecla.next().toUpperCase());
+                while ((linea = br.readLine()) != null) {
+                    String[] contacto = linea.split(",");
+                    Persona p = new Persona(contacto[0], contacto[1], contacto[2], contacto[3], contacto[4], contacto[5]);
+                    if (buscacedula.equals(contacto[0])) {
+                        p.desplegar();
+                        x = 1;
+                        break;
+                    }
 
-    
-    
+                }
+                if (x == 0) {
+                    System.out.println("Registro Médico no existe: ");
+
+                }
+            } else {
+                System.out.println("Agenda de Médicos vacía");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
 }
