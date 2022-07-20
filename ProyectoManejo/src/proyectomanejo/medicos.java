48e8868fc35300779e20f084ee3a47241593d602
Lizard <Lizard@Lizard>
2022-07-19 19:11:41 -0500

    Se creo los atributos de la clase medicos.

--- ProyectoManejo/src/proyectomanejo/medicos.java
+++ ProyectoManejo/src/proyectomanejo/medicos.java
@@ -1,14 +1,78 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectomanejo;
 
 /**
  *
  * @author Lizard
  */
+import java.util.Scanner;
+
 public class medicos {
+
+    //atributos
+    String cedula;
+    String nombre;
+    String apellido;
+    String direccion;
+    String telefono;
+    String especialidad;
+    String otro;
+    Boolean ot;
+
+    //cédula
+    public void setingresocedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getingresocedula() {
+        return cedula;
+    }
+
+    //nombre
+    public void setingresonombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getingresonombre() {
+        return nombre;
+    }
+
+    //apellido
+    public void setingresoapellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getingresoapellido() {
+        return apellido;
+    }
+
+    //direccion
+    public void setingresodireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getingresodireccion() {
+        return direccion;
+    }
+
+    //telefono
+    public void setingresotelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getingresotelefono() {
+        return telefono;
+    }
+
+    //especialidad
+    public void setingresoespecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getingresoespecialidad() {
+        return especialidad;
+    }
+
     
 }
+
