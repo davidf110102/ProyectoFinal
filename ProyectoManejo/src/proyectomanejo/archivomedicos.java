b9612d39056bab95bf6930dc001b9d8e587241fb
Lizard <Lizard@Lizard>
2022-07-19 19:31:48 -0500

    Se creo el metodo escribirpersona  para el registro de medicos

--- ProyectoManejo/src/proyectomanejo/archivomedicos.java
+++ ProyectoManejo/src/proyectomanejo/archivomedicos.java
@@ -5,11 +5,40 @@
  */
 package proyectomanejo;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 /**
  *
  * @author Lizard
  */
 public class archivomedicos {
+    
+            public void escribirpersona(Persona nueva) {
+        try {
+            File f = new File("medicos.txt");
+            FileWriter fw;
+            BufferedWriter bw;
+
+            if (f.exists()) {
+                fw = new FileWriter(f, true);
+                bw = new BufferedWriter(fw);
+                bw.newLine();
+                bw.write(nueva.cedula + "," + nueva.nombre + "," + nueva.apellido + "," + nueva.direccion + "," + nueva.telefono + "," + nueva.especialidad);
+            } else {
+                fw = new FileWriter(f, true);
+                bw = new BufferedWriter(fw);
+                bw.write(nueva.cedula + "," + nueva.nombre + "," + nueva.apellido + "," + nueva.direccion + "," + nueva.telefono + "," + nueva.especialidad);
+            }
+            bw.close();
+            fw.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
     
     
 }
