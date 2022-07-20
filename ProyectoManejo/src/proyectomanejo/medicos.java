9087686b891be6c644fa264537d3f99cd3f438ad
Lizard <Lizard@Lizard>
2022-07-19 19:13:24 -0500

    Se realizo el metodo personal_medico para el control del ingreo de datos

--- ProyectoManejo/src/proyectomanejo/medicos.java
+++ ProyectoManejo/src/proyectomanejo/medicos.java
@@ -73,6 +73,51 @@ public class medicos {
         return especialidad;
     }
 
-    
+    public void personal_medico() {
+        Scanner tecla = new Scanner(System.in);
+        System.out.printf("\n");
+        System.out.printf("%53s\n", "\033[35;1m M E D I C O S");
+        Boolean ce = true;
+        //CONTROL DE LA CEDULA
+        Integer num;
+        while (ce = true) {
+            System.out.printf("%51s", "\033[34;1m Cédula:       ");
+            setingresocedula(tecla.next().toUpperCase());
+            num = cedula.length();
+            if (num == 10) {
+                ce = false;
+                break;
+            } else {
+                System.out.printf("%55s", "\033[31;1m CÉDULA INCORRECTA\n");
+                System.out.printf("\n");
+            }
+        }
+         //FIN CONTROL CEDULA
+        
+        
+       //CONTROL DE DATOS PARA MEDICOS
+        System.out.printf("%51s", "\033[34;1m Nombre:       ");
+        setingresonombre(tecla.next().toUpperCase());
+        System.out.printf("%51s", "\033[34;1m Apellido:     ");
+        setingresoapellido(tecla.next().toUpperCase());
+        System.out.printf("%51s", "\033[34;1m Dirección:    ");
+        setingresodireccion(tecla.next().toUpperCase());
+        while (ce = true) {
+            System.out.printf("%51s", "\033[34;1m Teléfono:     ");
+            setingresotelefono(tecla.next().toUpperCase());
+            num = telefono.length();
+            if (num == 10) {
+                ce = false;
+                break;
+            } else {
+                System.out.printf("%55s", "\033[31;1m NÚMERO INCORRECTA\n");
+                System.out.printf("\n");
+            }
+        }
+
+        System.out.printf("%51s", "\033[34;1m Especialidad: ");
+        setingresoespecialidad(tecla.next().toUpperCase());
+    }
+
 }
 
